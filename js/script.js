4937d77859f8532766be605e1e88e2c312504182
Willians Narciso <williansnarcizzo@gmail.com>
2026-01-12 21:10:05 -0300

    refactor: replace alert validation with inline error message

--- js/script.js
+++ js/script.js
@@ -19,13 +19,29 @@ const currencyName = document.querySelector("#currency-name")
 // Imagem da moeda
 const currencyImage = document.querySelector(".currency-img")
 
+<<<<<<< HEAD
 //Mensagem de erro
 const errorMessage = document.querySelector (".error-message")
+=======
+const errorMessage = document.querySelector(".error-message")
+>>>>>>> e83bb65 (refactor: replace alert validation with inline error message)
 
 // Taxas de conversão (base: dólar)
-const rates = {
-  euro: 0.92,
-  real: 5.10
+const currencies = {
+  EUR: {
+name: "Euro",
+rate: 0.92,
+locale: "de-DE",
+currency: "EUR",
+image: "./assets/euro.png"
+},
+BRL:{
+  name:"Real Brasileiro",
+  rate: 5.10,
+  locale: "pt-BR",
+  currency: "BRL",
+  image: "./assets/brasil.png"
+}
 }
 
 // Função para formatar valores em moeda
@@ -42,7 +58,10 @@ function showError(message){
 // Função principal de conversão
 function convertValues() {
   const inputValue = Number(inputCurrency.value)
+  const selectedCurrency = currencySelect.value
+  const currencyData = currencies [selectedCurrency]
 
+<<<<<<< HEAD
   //Limpa erro
   function clearError(){
     errorMessage.classList.remove("active")
@@ -83,23 +102,48 @@ function convertValues() {
       "BRL"
     )
   }
+=======
+if (!currencyData) return
+
+  //Validação
+if (!inputValue || inputValue <=0){
+  errorMessage.innerText = "Digite um valor Válido"
+  errorMessage.style.display = "block"
+  return
+}else{
+  errorMessage.style.display = "none"
+>>>>>>> e83bb65 (refactor: replace alert validation with inline error message)
 }
+
+//Valor em dólar
+currencyValueToConvert.innerHTML = formatCurrency (
+  inputValue,
+  "en-US",
+  "USD"
+)
+
+//Conversão
+const convertedValue = inputValue * currencyData.rate
+
+currencyValueConverted.innerHTML = formatCurrency(
+  convertedValue,
+  currencyData.locale,
+  currencyData.currency
+)
+}
+
 
 // Atualiza nome e imagem da moeda
 function changeCurrency() {
-  if (currencySelect.value === "Euro") {
-    currencyName.innerHTML = "Euro"
-    currencyImage.src = "./assets/euro.png"
-  }
+  const selectedCurrency = currencySelect.value
+  const currencyData = currencies [selectedCurrency]
 
-  if (currencySelect.value === "Real") {
-    currencyName.innerHTML = "Real Brasileiro"
-    currencyImage.src = "./assets/brasil.png"
-  }
+  currencyName.innerHTML = currencyData.name
+  currencyImage.src= currencyData.image
 
   convertValues()
 }
 
-// Eventos
 currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
+
